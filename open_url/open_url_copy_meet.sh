60e6a59d26c3b8ae4c751a2561a16b011a300ca9
Vincent Ballet <vincent.ballet@me.com>
2020-12-04 16:00:10 +0100

    Add permanent google meet

---
+++ open_url/open_url_copy_meet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Google Meet
+# @raycast.packageName Copy + Open URL
+# @raycast.mode silent
+# @raycast.author VincentBallet
+
+# Optional parameters:
+# @raycast.icon 👋
+
+echo "https://meet.google.com/icg-xviz-xos" | pbcopy
+open "https://meet.google.com/icg-xviz-xos"
